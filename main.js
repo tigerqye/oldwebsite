8a30d0c843588aa7802e6e00705ab9b4224e0cc8
tigerqye <68398469+tigerqye@users.noreply.github.com>
2021-01-15 15:20:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const navBar = document.querySelector("#navagation");
 const stick = navBar.offsetTop;
+var load;
+var fade;
 
 window.onscroll = function() {whenScroll()};
 
@@ -20,4 +22,24 @@ function copyEmail(){
 	document.querySelector("#emailAddress").select();
 	document.execCommand("copy");
 	alert("Email Copied Successfully!")
+}
+
+function preload(){
+	fade = setTimeout(fadeaway, 1000);
+	load = setTimeout(show, 3000);
+}
+
+function fadeaway(){
+	var loading = document.querySelector("#loader");
+	loading.classList.add("fadeOut")
+}
+
+function show(){
+	var containers = document.querySelectorAll(".container, .withbackground");
+	var i;
+	for(i = 0; i < containers.length; i++)
+	{
+		containers[i].style.display = "block";
+	}
+	document.querySelector("#loader").style.display = "none";
 }
